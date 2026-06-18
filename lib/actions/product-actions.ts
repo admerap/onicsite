3ecdd92f167bba5021f5c4fd95d64f3d9ae9d4a8
ademir <ademirap3107@gmail.com>
2026-06-17 21:34:09 -0400

    Update project changes

--- lib/actions/product-actions.ts
+++ lib/actions/product-actions.ts
@@ -1,23 +1,24 @@
 "use server";
 
+import { Product } from "@/types";
 import prisma from "@/lib/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
-export async function getLatestProducts() {
+export async function getLatestProducts(): Promise<Product[]> {
   const data = await prisma.product.findMany({
     orderBy: {
       createdAt: "desc",
     },
     take: LATEST_PRODUCTS_LIMIT,
   });
-  return convertToPlainObject(data);
+  return convertToPlainObject(data) as unknown as Product[];
 }
 
 // Get single product by slug
-export async function getProductBySlug(slug: string) {
+export async function getProductBySlug(slug: string): Promise<Product | null> {
   const data = await prisma.product.findUnique({
     where: { slug: slug },
   });
-  return convertToPlainObject(data);
+  return convertToPlainObject(data) as unknown as Product | null;
 }
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
 function createPrismaClient() {
   const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL!,
   });
-  return new PrismaClient({ adapter });
+  return new PrismaClient({ adapter }).$extends({
+    result: {
+      product: {
+        price: {
+          compute(product) {
+            return product.price.toString();
+          },
+        },
+        rating: {
+          compute(product) {
+            return product.rating.toString();
+          },
+        },
+      },
+    },
+  });
 }
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient>;
+};
 
 const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
